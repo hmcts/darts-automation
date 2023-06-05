a37f4225dd9323ea831e1fd1cde8985dce310a5c
Trevor <trevor.shaw@cgi.com>
2023-06-05 16:00:00 +0100

    DMP-388 more utils

---
+++ lib/src/test/java/uk/gov/hmcts/darts/automation/cucumber/testRunner/TestRunner_Cucumber.java
@@ -0,0 +1,27 @@
+package uk.gov.hmcts.darts.automation.cucumber.testRunner;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.IncludeTags;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+//import io.cucumber.junit.platform.engine.Cucumber;
+
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.*;
+
+@Suite
+@IncludeEngines("cucumber")
+@IncludeTags(value = "Regression | DMP-407")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "html:target/html/results.html")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "uk/gov/hmcts/darts/automation/cucumber/steps")
+@SelectClasspathResource(value = "cucumber/features")
+public class TestRunner_Cucumber {
+}
+/*
+ * try
+ * 
+*/
