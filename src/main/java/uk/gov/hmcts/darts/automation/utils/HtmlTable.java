bc1bfa29fc35242000f47eb12a61662434169ae1
Trevor Shaw <135220433+trev-hmcts@users.noreply.github.com>
2023-10-31 14:39:22 +0000

    DMP-862 changes to htmltabledata class

--- src/main/java/uk/gov/hmcts/darts/automation/utils/HtmlTable.java
+++ src/main/java/uk/gov/hmcts/darts/automation/utils/HtmlTable.java
@@ -32,34 +32,16 @@ public class HtmlTable {
     public void verifyHtmlTableData(DataTable dataTable, boolean isFirstRowHeader) {
         NAV.waitForPageLoad();
         WebElement htmlTableElement = webDriver.findElement(By.cssSelector(".govuk-table"));
-        List<WebElement> rowElements = htmlTableElement.findElements(By.tagName("tr"));
-        List<List<String>> dataTableRows = dataTable.asLists(); //outer List<> is rows, inner List<> is cells
-
-        // Check number of rows in Datatable and html table are equal
-        Assert.assertEquals("Datatable rows should match the HtmlTable rows", dataTableRows.size(), rowElements.size());
-        errorCount = 0;
-
-        //Verify table header
-        if (isFirstRowHeader) {
-            List<WebElement> headerElements = rowElements.get(0).findElements(By.xpath(".//th")); //get all the headers from the row WebElement
-            compareTableData(headerElements, dataTableRows.get(0), 0, rowElements.get(0).findElements(By.xpath(".//th")).size());
-            rowElements.remove(0);
-        }
-
-        int startIndex = isFirstRowHeader ? 1 : 0; // Skip the first row if it's a header
-        for (int i = startIndex; i <= rowElements.size(); i++) {
-            List<String> dataTableColumns = dataTableRows.get(i);
-            WebElement rowElem = rowElements.get(i - startIndex);
-            List<WebElement> cellElements = rowElem.findElements(By.xpath(".//td"));
-            compareTableData(cellElements, dataTableColumns, i, rowElem.findElements(By.xpath(".//td")).size());
-        }
-        Assert.assertEquals(0, errorCount);
-        log.error("Html Table and Datatable has {} error count", errorCount);
+		verifyHtmlTableData(dataTable, isFirstRowHeader, htmlTableElement);
     }
 
     public void verifyHtmlTableData(DataTable dataTable, boolean isFirstRowHeader, String tablename) {
         NAV.waitForPageLoad();
         WebElement htmlTableElement = webDriver.findElement(By.xpath("//*[text()=\""+tablename+"\"]/following::table[1]"));
+		verifyHtmlTableData(dataTable, isFirstRowHeader, htmlTableElement);
+	}
+
+    public void verifyHtmlTableData(DataTable dataTable, boolean isFirstRowHeader, WebElement htmlTableElement) {
         List<WebElement> rowElements = htmlTableElement.findElements(By.tagName("tr"));
         List<List<String>> dataTableRows = dataTable.asLists(); //outer List<> is rows, inner List<> is cells
 
