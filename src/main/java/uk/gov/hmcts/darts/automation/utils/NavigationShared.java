a81bfc0bc34d91ad5f63b1f96ec3e33380260c55
Trevor Shaw <135220433+trev-hmcts@users.noreply.github.com>
2023-11-16 13:31:44 +0000

    Update NavigationShared.java

--- src/main/java/uk/gov/hmcts/darts/automation/utils/NavigationShared.java
+++ src/main/java/uk/gov/hmcts/darts/automation/utils/NavigationShared.java
@@ -1937,13 +1937,4 @@ public class NavigationShared {
 
 	}
 
-	public NavigationShared set_value_clickaway(String location_name, String value) throws Exception {
-		log.info("About to Set input field with label =>" + location_name + "<= to =>" + value);
-		WebElement targetElement = findInputFieldByLabelText(location_name);
-		String substitutedValue = setElementValueTo(targetElement, value);
-		log.info("Set input field with label =>" + location_name + "<= to =>" + substitutedValue);
-		clickAway(targetElement);
-		return this;
-	}
-
 }
