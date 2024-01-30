31a9c3da8154076c439c98672bcdcbe8933df7dc
Trevor Shaw <135220433+trev-hmcts@users.noreply.github.com>
2024-01-30 12:48:32 +0000

    DMP-1467_minor updates and fixes - database host & password fix (#173)
    * DMP-1467 date/time separate in some apis
    
    * DMP-1467 add audios json api
    
    * DMP-1467 fix link for dynamic data
    
    * DMP-1467 more changes for demo
    
    * DMP-1467 correct demo changes
    
    * DMP-1467 reduce events in smoketest
    
    * DMP-1467 remove more events
    
    * DMP-1467 move tags
    
    * DMP-1467 remove smoketest tag from event api
    
    * DMP-1467 database parameter access change
    
    * DMP-1467 update access to AUTOMATION_PASSWORD

--- src/main/java/uk/gov/hmcts/darts/automation/utils/ReadProperties.java
+++ src/main/java/uk/gov/hmcts/darts/automation/utils/ReadProperties.java
@@ -59,12 +59,12 @@ public class ReadProperties {
 	public static String apiDbSchema = getSystemValue("DARTS_API_DB_SCHEMA");
 //	n.b.  System.getProperty("AZURE_STORAGE_CONNECTION_STRING"); defined but not used
 	public static String apiDbUserName = getSystemValue("DARTS_API_DB_USERNAME");
-	public static String apiDbPassword = runLocal ? getSystemValue(main("DARTS_API_DB_PASSWORD")) : getSystemValue("DARTS_API_DB_PASSWORD");
+	public static String apiDbPassword = runLocal ? getSystemValue(main("DB_PASSWORD_KEY")) : getSystemValue("DARTS_API_DB_PASSWORD");
 	public static String apiDbPort = getSystemValue("DARTS_API_DB_PORT");
-	public static String apiDbHost = runLocal ? getSystemValue(main("DARTS_API_DB_HOST")) : getSystemValue("DARTS_API_DB_HOST");
+	public static String apiDbHost = runLocal ? getSystemValue(main("DB_HOST_KEY")) : getSystemValue("DARTS_API_DB_HOST");
 	public static String apiDbDatabase = getSystemValue("DARTS_API_DB_DATABASE");
 	public static String automationUserId = getSystemValue("AUTOMATION_USERNAME");
-	public static String automationPassword = runLocal ? getSystemValue(main("AUTOMATION_PASSWORD")) : getSystemValue("AUTOMATION_PASSWORD");
+	public static String automationPassword = getSystemValue("AUTOMATION_PASSWORD");
 
 	public static String automationTranscriberUserId = getSystemValue("AUTOMATION_TRANSCRIBER_USERNAME");
 	public static String automationLanguageShopTestUserId = getSystemValue("AUTOMATION_LANGUAGE_SHOP_TEST_USERNAME");
