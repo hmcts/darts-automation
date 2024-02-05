bbfc72c8af741faeafd17dec845742cdf773aaa2
preskshajain <preksha.jain@hmcts.net>
2024-02-05 14:33:15 +0000

    Scenario for Requester

--- src/test/java/uk/gov/hmcts/darts/automation/cucumber/steps/StepDef_portal.java
+++ src/test/java/uk/gov/hmcts/darts/automation/cucumber/steps/StepDef_portal.java
@@ -118,7 +118,7 @@ public class StepDef_portal extends StepDef_base {
         portal.uploadDocument(filename, uploadLabel);
     }
 
-    @Then("I wait for \"([^\"]*)\" minutes for the audio file with start time \"([^\"]*)\" to appear for \"([^\"]*)\"$")
+    @Then("I wait for \"([^\"]*)\" minutes with \"([^\"]*)\" to appear for \"([^\"]*)\"$")
     public void waitForAudioFileWithStartTime(String waitTime, String startTime, String caseNumber) {
         portal.waitForAudioFile(waitTime, startTime, caseNumber);
     }
