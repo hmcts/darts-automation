5e08b96a73e23db0cf0b0819e702df197e58422a
Dave Thompson <david.thompson@hmcts.net>
2024-02-07 09:05:58 +0000

    Jenkins enhancements, including reports and running specific tags (#183)
    * Adding reports to jenkins
    
    * Enhancing to allow different tags to be run nightly
    
    - tags are injected as env vars
    - changes to build.gradle to read and apply the tags
    - default is "smoketest"
    - need to update Jenkinsfile_nightly with the tags required
    
    * Amending test results location, to appease Jenkins

--- src/test/java/uk/gov/hmcts/darts/automation/cucumber/testRunner/TestRunner_Cucumber.java
+++ src/test/java/uk/gov/hmcts/darts/automation/cucumber/testRunner/TestRunner_Cucumber.java
@@ -24,11 +24,10 @@ import static io.cucumber.junit.platform.engine.Constants.EXECUTION_DRY_RUN_PROP
 
 @Suite
 @IncludeEngines("cucumber")
-@IncludeTags(value = "smoketest")
 //@IncludeTags(value = "smoketest | DMP-407")
 @ExcludeTags(value = "broken | obsolete")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "html:reports/html/results.html, junit:reports/junit/cucumber.xml")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "html:test-results/html/results.html, junit:test-results/functionalTest/cucumber.xml")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "uk/gov/hmcts/darts/automation/cucumber/steps")
 @ConfigurationParameter(key = SNIPPET_TYPE_PROPERTY_NAME, value = "camelcase")
 @ConfigurationParameter(key = EXECUTION_DRY_RUN_PROPERTY_NAME, value = "false")
