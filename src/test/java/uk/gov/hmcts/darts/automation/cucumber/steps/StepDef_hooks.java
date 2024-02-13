a9e0b19185051cb97009e3ad28eb738211c50299
Trevor Shaw <135220433+trev-hmcts@users.noreply.github.com>
2024-02-13 09:17:18 +0000

    DMP-2258 run details change

--- src/test/java/uk/gov/hmcts/darts/automation/cucumber/steps/StepDef_hooks.java
+++ src/test/java/uk/gov/hmcts/darts/automation/cucumber/steps/StepDef_hooks.java
@@ -110,7 +110,7 @@ public class StepDef_hooks extends StepDef_base {
 		if (!ReadProperties.buildNo.equals(build)) {
 			log.warn("Automation started with build {} but is now Build {}", ReadProperties.buildNo, build);
 		}
-		scenario.log("Environment: " + ReadProperties.environment + ", Build: " + ReadProperties.buildNo);
+		scenario.attach("Environment: " + ReadProperties.environment + ", Build: " + ReadProperties.buildNo + ", seq: " + ReadProperties.seq, "text/plain", "Run-details");
 		testdata.setProperty("started", DateUtils.timestamp());
 	}
 	
